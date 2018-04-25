5b740b56484002eb074385a6cdd464492278919f
Dmitry <rundle@yandex.ru>
2018-04-25 14:36:32 +0500

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: 'Gatsby Default Starter',
     desc: 'A new blog',
   },
-  pathPrefix: '/gatsby',
+  pathPrefix: '/gatsbyblog',
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
